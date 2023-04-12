08668220c3cae9110661761fd794dba2f8a9039d
Avanishsri31 <90971881+Avanishsri31@users.noreply.github.com>
2023-04-12 23:59:16 +0530

    api updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://stack-overflow-backend-nz2g.onrender.com'})
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('Profile')){
